3e659f4d04c5605cd22c2327eb7d4463d883c026
oliceglad <vycheslavofficial@gmail.com>
2023-08-07 14:18:45 +0400

    changed url

--- parser/parsewb.py
+++ parser/parsewb.py
@@ -22,6 +22,6 @@ woman_clothes = {
 class ParseWB:
     @staticmethod
     def get_category(category: str):
-        url = f'https://catalog.wb.ru/catalog/men_clothes1/catalog?appType=1&cat={man_clothes["man_clothes_cat"][category]}&curr=rub&dest=-1257786&regions=80,38,83,4,64,33,68,70,30,40,86,75,69,22,1,31,66,110,48,71,114&sort=popular&spp=0'
+        url = f'https://catalog.wb.ru/catalog/{man_clothes["man_clothes_cat"][category]["tag"]}/catalog?appType=1&cat={man_clothes["man_clothes_cat"][category]["cat"]}&curr=rub&dest=-1257786&regions=80,38,83,4,64,33,68,70,30,40,86,75,69,22,1,31,66,110,48,71,114&sort=popular&spp=0'
         response = requests.get(url=url)
         return response.json()
